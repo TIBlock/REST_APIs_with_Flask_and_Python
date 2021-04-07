09fab66df03597982d416e5086279573aabe539b
TIBlock <taylorblocker@gmail.com>
2021-04-06 19:38:59 -0500

    comitting sqlite3 changes

--- section_5_files/test.py
+++ section_5_files/test.py
@@ -2,3 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect('data.db')
 
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users (id int, username text, password text)"
+cursor.execute(create_table)
+
+user = (1, 'jose', 'asdf')
+insert_query = "INSERT INTO"
