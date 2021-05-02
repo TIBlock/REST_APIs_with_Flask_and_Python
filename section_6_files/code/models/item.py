54f5bc157ecfaf52d5647ee5ca9c8cdd63418227
Taylor Blocker <taylorblocker@gmail.com>
2021-05-02 14:29:16 -0500

    updated item model and resource files

--- section_6_files/code/models/item.py
+++ section_6_files/code/models/item.py
@@ -1,3 +1,12 @@
+import sqlite3
+
+class ItemModel:
+    def __init__(self, name, price):
+    self.name = name
+    self.price = price
+
+    def json(self):
+    return {'name': self.name, 'price': self.price}
 
     @classmethod
     def find_by_name(cls, name):
@@ -10,15 +19,24 @@
         connection.close()
 
         if row:
-            return {'item': {'name': row[0], 'price': row[1]}}
+            return cls(*row)
 
-    @classmethod
-    def insert(cls, item):
+    def insert(self):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
         query = ("INSERT INTO items VALUES(?, ?)")
-        cursor.execute(query, (item['name'], item['price']))
+        cursor.execute(query, (self.name, self.price))
+
+        connection.commit()
+        connection.close()
+
+    def update(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (self.price, self.name))
 
         connection.commit()
         connection.close()
