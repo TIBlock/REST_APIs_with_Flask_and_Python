db196ea06a30e799c42f5205b0c5917ae737ed0d
Taylor Blocker <taylorblocker@gmail.com>
2021-05-02 13:50:22 -0500

    building out resource and model files

--- section_6_files/code/app.py
+++ section_6_files/code/app.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import ItemList, Item
+from resouces.user import UserRegister
+from resources.item import ItemList, Item
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
